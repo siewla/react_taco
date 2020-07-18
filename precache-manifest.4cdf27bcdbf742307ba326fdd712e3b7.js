99d316fe3309c016df018c1b38530e541b67e3dd
siewla <siewlapang@gmail.com>
2020-07-18 11:26:23 +0800

    Updates

---
+++ precache-manifest.4cdf27bcdbf742307ba326fdd712e3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa5b0f2115751750e6bc18b0be5f0a4",
+    "url": "/react_taco/index.html"
+  },
+  {
+    "revision": "290d5c865fe5286a7947",
+    "url": "/react_taco/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6cc7cdcc7b2f140ab4fb",
+    "url": "/react_taco/static/js/2.0aa6a2a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_taco/static/js/2.0aa6a2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290d5c865fe5286a7947",
+    "url": "/react_taco/static/js/main.702f39fa.chunk.js"
+  },
+  {
+    "revision": "26a03f82f05c526c0583",
+    "url": "/react_taco/static/js/runtime-main.c42bc93c.js"
+  }
+]);
---
+++ static/js/main.702f39fa.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact_taco=this.webpackJsonpreact_taco||[]).push([[0],[,,,function(e,t,n){e.exports=n(10)},,,,,function(e,t,n){},function(e,t,n){},function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a),r=n(2),o=n.n(r);n(8),n(9);var l=function(){return c.a.createElement("div",null,c.a.createElement("h1",null,"hello taco"))};o.a.render(c.a.createElement(c.a.StrictMode,null,c.a.createElement(l,null)),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.702f39fa.chunk.js.map
